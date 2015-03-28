c76da2f167916befd1d68fdc6e447f062c74fc73
jhuang0416 <jhuang0416@gmail.com>
2015-03-27 21:53:13 -0400

    Final draft

--- NTU_beginner_AdvExercises/Question5.sql
+++ NTU_beginner_AdvExercises/Question5.sql
@@ -1,22 +1,45 @@
-/* 5-FUNCTION: Write a function to compute the rental price. */
+DROP VIEW rental_prices;
 
 DELIMITER //
 
-CREATE FUNCTION rental_prices(end_date DATE, start_date DATE, daily_rate DECIMAL(6,2),discount DOUBLE) 
+CREATE FUNCTION rental_price(start_date DATE, end_date DATE, daily_rate DECIMAL(6,2), discount DOUBLE)
 RETURNS DECIMAL(8,2) NOT DETERMINISTIC
-
 BEGIN
+
 DECLARE Price DECIMAL(8,2);
-IF DATEDIFF(end_date,start_date) < 7 THEN
-SET Price = DATEDIFF(end_date,start_date)*daily_rate*(1-discount);
-ELSE
-SET Price = DATEDIFF(end_date,start_date)*daily_rate*(1-discount)*0.8;
+
+IF DATEDIFF(end_date, start_date) < 7 THEN
+	SET Price = DATEDIFF(end_date, start_date)*daily_rate*(1-discount);
+ELSE 
+	SET Price = DATEDIFF(end_date, start_date)*daily_rate*((1-discount)*0.8);
 END IF;
-RETURN (Price);
-END //
 
-DELIMITER ; 
+RETURN(Price);
 
-Select rental_price('2014-09-23', '2014-09-13', 99.99, 0.1);
+END//
 
-Select * from rental_prices \G 
+
+/* Reconstruction of rental_price view to accomodate the rental_prices function. */
+
+CREATE VIEW rental_prices
+AS
+SELECT
+   v.veh_reg_no    AS `Vehicle No`,
+   v.daily_rate    AS `Daily Rate`,
+   c.name          AS `Customer Name`,
+   c.discount*100  AS `Customer Discount (%)`,
+   r.start_date    AS `Start Date`,
+   r.end_date      AS `End Date`,
+   DATEDIFF(r.end_date, r.start_date) AS `Duration`,
+   -- Compute the rental price
+   -- Preferred customer has discount, 20% discount for 7 or more days
+   -- CAST the result from DOUBLE to DECIMAL(8,2)
+   CAST(
+      IF (DATEDIFF(r.end_date, r.start_date) < 7,
+          DATEDIFF(r.end_date, r.start_date)*daily_rate*(1-discount),
+          DATEDIFF(r.end_date, r.start_date)*daily_rate*(1-discount)*0.8)
+      AS DECIMAL(8,2)) AS price,
+(SELECT rental_price(r.start_date, r.end_date, v.daily_rate, c.discount)) As 'Rental Price'
+FROM rental_records AS r
+   INNER JOIN Vehicles  AS v USING (veh_reg_no)
+   INNER JOIN Customers AS c USING (customer_id);
--- NTU_beginner_AdvExercises/Question9.sql
+++ NTU_beginner_AdvExercises/Question9.sql
@@ -12,49 +12,76 @@ price *= (1-perfered_customer_discount);
 
 */
 
-CREATE TABLE public_holiday (
-`date` DATE NOT NULL,
-`description` VARCHAR(30) NOT NULL
-)ENGINE=InnoDB;
-
-INSERT INTO public_holiday VALUES 
-('2014-12-25','Christmas Day');
-
-
-DELIMITER //
-
-CREATE FUNCTION rental_price_hol (start_date DATE, end_date DATE, daily_rate DECIMAL(6,2),discount DOUBLE) 
+CREATE FUNCTION Public_holiday_prices(start_date DATE, end_date DATE, daily_rate DECIMAL(6,2), discount DOUBLE)
 RETURNS DECIMAL(8,2) NOT DETERMINISTIC
+BEGIN
 
-BEGIN 
-DECLARE price DECIMAL(8,2);
-DECLARE test_date DATE;
-DECLARE public_holiday DATE;
-SET test_date = start_date;
-SET price = 0;
-SET @public_holiday = (SELECT date FROM public_holiday WHERE description = 'Christmas Day'); 
+DECLARE Price DECIMAL(8,2) DEFAULT 0.00;
+DECLARE Offset_Row INT DEFAULT 0;
 
-REPEAT
+SET @Test_date := start_date;
+SET @Public_holiday := (SELECT `date` FROM public_holiday LIMIT Offset_Row, 1); --get one record at a time from the public_holiday table
+SET @Count1 := (SELECT COUNT(*) FROM public_holiday);  --The total rows in the public_holiday table
 
-IF (DAYOFWEEK(test_date) = 1 OR 7) OR (test_date = @public_holiday)
-THEN SET price = price + daily_rate;
-ELSE 
-SET price = price + (daily_rate*(1-discount));
-END IF;
+Comparing_Computing: LOOP
 
-SET test_date = DATE_ADD(test_date,INTERVAL 1 DAY);
-UNTIL test_date > end_date
-END REPEAT;
-
-IF (DATEDIFF(end_date,start_date)) >= 7
-THEN SET price = price*(1-discount)*0.8;
+--Calculating the total rental price while determining each rental date landed on weekdays, weekends, or public holiday
+IF ((DAYOFWEEK(date(@Test_date))) = 1 OR 7) OR ((EXTRACT(MONTH FROM date(@Test_date)) = EXTRACT(MONTH FROM date(@Public_holiday))) 
+	AND (EXTRACT(DAY FROM date(@Test_date)) = EXTRACT(DAY FROM date(@Public_holiday)))) 
+THEN 
+SET Price = Price + daily_rate;
 ELSE
-SET price = price*(1-discount);
+SET Price = Price + (daily_rate * (1 - discount));
+END IF;
+SET Offset_Row = Offset_Row + 1;
+--Testing whether there is more records for public_holiday table
+IF (Offset_Row != @Count1)
+THEN 
+ITERATE Comparing_Computing;
+END IF;
+SET Offset_Row = 0; --Return to the beginning of the public_holiday table
+SET @Test_date := DATE_ADD(date(@Test_date), INTERVAL 1 DAY); --Proceed to the next date to be compared
+--Determining whether the next date is within the range of the rental period
+IF (date(@Test_date) BETWEEN start_date AND end_date)
+THEN
+ITERATE Comparing_Computing;
+END IF;
+LEAVE Comparing_Computing; --Leave the loop once done 
+END LOOP Comparing_Computing;
+
+--Computing the total rental price with discount based on the duration of the rental period
+IF (DATEDIFF(end_date,start_date)) >= 7
+THEN SET price = price * (1 - discount) * 0.8;
+ELSE
+SET price = price * (1 - discount);
 END IF;
 
-RETURN(price); 
+RETURN Price;
 END // 
 
-DELIMITER ;
 
-SELECT rental_price_hol('2014-10-12','2014-10-18',119.99,0.10); 
+CREATE VIEW rental_prices
+AS
+SELECT
+   v.veh_reg_no    AS `Vehicle No`,
+   v.daily_rate    AS `Daily Rate`,
+   c.name          AS `Customer Name`,
+   c.discount*100  AS `Customer Discount (%)`,
+   r.start_date    AS `Start Date`,
+   r.end_date      AS `End Date`,
+   DATEDIFF(r.end_date, r.start_date) AS `Duration`,
+   CAST(
+      IF (DATEDIFF(r.end_date, r.start_date) < 7,
+          DATEDIFF(r.end_date, r.start_date)*daily_rate*(1-discount),
+          DATEDIFF(r.end_date, r.start_date)*daily_rate*(1-discount)*0.8)
+      AS DECIMAL(8,2)) AS price,
+(SELECT rental_price(r.start_date, r.end_date, v.daily_rate, c.discount)) As 'Rental Price',
+   SUM(p.amount) AS 'Total Paid Amount',
+(SELECT rental_price(r.start_date, r.end_date, v.daily_rate, c.discount)-SUM(p.amount)) AS 'Outstanding Balance',
+(SELECT Public_holiday_prices(start_date, end_date, daily_rate, discount))AS 'Public_holiday_Wkend'
+FROM rental_records AS r
+   INNER JOIN Vehicles  AS v USING (veh_reg_no)
+   INNER JOIN Customers AS c USING (customer_id)
+   INNER JOIN Payments AS p USING (rental_id)
+   GROUP BY rental_id; 
+
